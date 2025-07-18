eca2b588ca2b18d3a6dfe2b67894a4fa9b78b67e
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-07-18 23:23:33 +0500

    Notify all clients of change of TotalUsers.

--- SignalRSample/Hubs/UserHub.cs
+++ SignalRSample/Hubs/UserHub.cs
@@ -7,15 +7,17 @@ namespace SignalRSample.Hubs
 		public static int TotalViews { get; set; } = 0;
 		public static int TotalUsers { get; set; } = 0;
 
-		public override Task OnConnectedAsync()
+		public override async Task<Task> OnConnectedAsync()
 		{
 			TotalUsers++;
+			await Clients.All.SendAsync("UpdateTotalUsers", TotalUsers);
 			return base.OnConnectedAsync();
 		}
 
-		public override Task OnDisconnectedAsync(Exception? exception)
+		public override async Task<Task> OnDisconnectedAsync(Exception? exception)
 		{
 			TotalUsers--;
+			await Clients.All.SendAsync("UpdateTotalUsers", TotalUsers);
 			return base.OnDisconnectedAsync(exception);
 		}
 
