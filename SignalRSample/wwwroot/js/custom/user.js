b296695ce06ff17cb125eb9bf28482fbbace150f
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-07-18 17:19:19 +0500

    SignalR connection and method implementation added in user.js

---
+++ SignalRSample/wwwroot/js/custom/user.js
@@ -0,0 +1,24 @@
+﻿// Create Connection
+var connection = new signalR.HubConnectionBuilder().withUrl("/hubs/user").Build();
+
+// Invoke HUB methods AKA Send Notification to HUB
+function WindowLoaded() {
+	connection.send("AddView");
+}
+
+// Connect to methods that HUB invokes AKA Receive Notification from HUB
+connection.on("UpdateTotalViews", (totalViews) => {
+	$('#totalViews').text(totalViews);
+})
+
+// Start Connection
+connection.start().then(OnSuccessConnection, OnFailedConnection);
+
+function OnSuccessConnection() {
+	console.log("Successfully connected to HUB");
+	WindowLoaded();
+}
+
+function OnFailedConnection() {
+	console.log("Failed to connect to HUB");
+}
