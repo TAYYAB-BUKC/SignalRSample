d8e36cee5113dc8263395a81b3570c3beb5c78e2
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-07-18 16:43:27 +0500

    AddView implemented in UserHub.

--- SignalRSample/Hubs/UserHub.cs
+++ SignalRSample/Hubs/UserHub.cs
@@ -4,5 +4,13 @@ namespace SignalRSample.Hubs
 {
 	public class UserHub : Hub
 	{
+		public static int TotalViews { get; set; } = 0;
+
+		public async Task AddView()
+		{
+			TotalViews++;
+			//send update to clients that view have been updated
+			await Clients.All.SendAsync("UpdateTotalViews", TotalViews);
+		}
 	}
 }
