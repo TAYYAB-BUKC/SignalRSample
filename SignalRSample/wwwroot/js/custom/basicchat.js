5fb5254fcd1547d3254f9894290f3d3f2644825e
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-07-20 15:51:44 +0500

    Basic chat JS implementation added.

---
+++ SignalRSample/wwwroot/js/custom/basicchat.js
@@ -0,0 +1,25 @@
+﻿var basicChatConnection = new signalR.HubConnectionBuilder().withUrl("/hubs/basicchat").build();
+
+$('#sendMessage').prop("disabled", true);
+
+basicChatConnection.on("NewMessageReceived", function (sender, message) {
+	$('#messagesList').append(`<li>${sender} - ${message}</li>`);
+});
+
+$('#sendMessage').on("click", function () {
+
+	var sender = $('#senderEmail').val();
+	var message = $('#chatMessage').val();
+
+	basicChatConnection.send("SendMessage", sender, message).catch(function (error) {
+		console.log(error);
+	});
+
+});
+
+basicChatConnection.start().then(function () {
+	console.log("Connected to Basic Chat Hub");
+	$('#sendMessage').prop("disabled", false);
+}, function () {
+	console.log("Failed to connect to Basic Chat Hub");
+});
