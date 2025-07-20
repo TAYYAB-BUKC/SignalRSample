f31466792cc612d2f05013ca0e88ccedbb067865
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-07-20 16:52:23 +0500

    Order JS implemented.

---
+++ SignalRSample/wwwroot/js/custom/order.js
@@ -0,0 +1,31 @@
+﻿var dataTable;
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        "ajax": {
+            "url": "/Order/GetAllOrder"
+        },
+        "columns": [
+            { "data": "id", "width": "5%" },
+            { "data": "name", "width": "15%" },
+            { "data": "itemName", "width": "15%" },
+            { "data": "count", "width": "15%" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `
+                        <div class="w-75 btn-group" role="group">
+                        <a href=""
+                        class="btn btn-primary mx-2"> <i class="bi bi-pencil-square"></i> </a>
+                      
+					</div>
+                        `
+                },
+                "width": "5%"
+            }
+        ]
+    });
+}
