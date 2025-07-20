4318a46c084547a791dcd49de8e19b98bdfb2c82
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-07-20 07:38:20 +0500

    Move all the basic methods implementation of SignalR with their scenarios from UserHub to BasicHub.

---
+++ SignalRSample/Hubs/BasicHub.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRSample.Hubs
+{
+	public class BasicHub : Hub
+	{
+		public override async Task<Task> OnConnectedAsync()
+		{
+			// If you need to send notification to each client
+			//await Clients.All.SendAsync("MethodName", parameter);
+
+			// If you need to send notification back to the request Initiator
+			//await Clients.Caller.SendAsync("MethodName", parameter);
+
+			// If you need to send notification to all the client except request Initiator
+			//await Clients.Others.SendAsync("MethodName", parameter);
+
+			// If you need to send notification to a particular client
+			//await Clients.Client("ConnectionIdOfClient").SendAsync("MethodName", parameter);
+
+			// If you need to send notification to particular clients
+			//await Clients.Clients("ConnectionIdOfClient", "ConnectionIdOfClient").SendAsync("MethodName", parameter);
+
+			// If you need to send notification to all clients except particular clients
+			//await Clients.AllExcept("ConnectionIdOfClient", "ConnectionIdOfClient").SendAsync("MethodName", parameter);
+
+			// If you need to send notification to all tabs of one client
+			//await Clients.User("UserIdOfClient").SendAsync("MethodName", parameter);
+
+			// If you need to send notification to particular users
+			//await Clients.Users("UserIdOfClient", "UserIdOfClient").SendAsync("MethodName", parameter);
+			return base.OnConnectedAsync();
+		}
+	}
+}
--- SignalRSample/Hubs/UserHub.cs
+++ SignalRSample/Hubs/UserHub.cs
@@ -29,27 +29,6 @@ namespace SignalRSample.Hubs
 			await Clients.All.SendAsync("UpdateTotalViews", TotalViews);
 
 			return $"Total Views of {parameter} : {TotalViews}";
-
-			// If you need to send notification back to the request Initiator
-			//await Clients.Caller.SendAsync("UpdateTotalViews", TotalViews);
-
-			// If you need to send notification to all the client except request Initiator
-			//await Clients.Others.SendAsync("UpdateTotalViews", TotalViews);
-
-			// If you need to send notification to a particular client
-			//await Clients.Client("ConnectionIdOfClient").SendAsync("UpdateTotalViews", TotalViews);
-
-			// If you need to send notification to particular clients
-			//await Clients.Clients("ConnectionIdOfClient", "ConnectionIdOfClient").SendAsync("UpdateTotalViews", TotalViews);
-
-			// If you need to send notification to all clients except particular clients
-			//await Clients.AllExcept("ConnectionIdOfClient", "ConnectionIdOfClient").SendAsync("UpdateTotalViews", TotalViews);
-
-			// If you need to send notification to all tabs of one client
-			//await Clients.User("UserIdOfClient").SendAsync("UpdateTotalViews", TotalViews);
-
-			// If you need to send notification to particular users
-			//await Clients.Users("UserIdOfClient", "UserIdOfClient").SendAsync("UpdateTotalViews", TotalViews);
 		}
 	}
 }
