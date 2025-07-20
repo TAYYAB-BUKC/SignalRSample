8b1adfb9bb0347d4ec6e52157760443c57d0867d
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-07-20 06:54:34 +0500

    VotingHub implementation added.

---
+++ SignalRSample/Hubs/VotingHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRSample.Hubs
+{
+	public class VotingHub : Hub
+	{
+		public Dictionary<string, int> VotingStatus()
+		{
+			return StaticDetails.StaticDetails.VoteCounts;
+		}
+	}
+}
