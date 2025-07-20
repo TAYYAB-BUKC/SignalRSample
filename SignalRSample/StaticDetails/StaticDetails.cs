194af8e960589ff5bdfde0637734b3dbcfdfa640
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-07-20 06:19:12 +0500

    StaticDetails implementation added for Voting.

---
+++ SignalRSample/StaticDetails/StaticDetails.cs
@@ -0,0 +1,20 @@
+﻿namespace SignalRSample.StaticDetails
+{
+	public static class StaticDetails
+	{
+		static StaticDetails()
+		{
+			VoteCounts = new Dictionary<string, int>
+			{
+				{ VoterA, 0 },
+				{ VoterB, 0 },
+				{ VoterC, 0 }
+			};
+		}
+
+		public static Dictionary<string, int> VoteCounts;
+		public const string VoterA = "Voter A";
+		public const string VoterB = "Voter B";
+		public const string VoterC = "Voter C";
+	}
+}
