d4985770570ae8b40ab558def24279fd06b1c45a
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-07-20 16:26:47 +0500

    Order DbSet added in ApplicationDbContext.

--- SignalRSample/Data/ApplicationDbContext.cs
+++ SignalRSample/Data/ApplicationDbContext.cs
@@ -1,13 +1,16 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using SignalRSample.Models;
 
 namespace SignalRSample.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+	public class ApplicationDbContext : IdentityDbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
+
+        public DbSet<Order> Orders { get; set; }
     }
 }
