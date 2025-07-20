42678ac42cece0fdc9030bc4a972ac1e4b153516
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-07-20 16:12:06 +0500

    SendPrivateMessage implemented in BasicChatHub on server side.

--- SignalRSample/Hubs/BasicChatHub.cs
+++ SignalRSample/Hubs/BasicChatHub.cs
@@ -1,12 +1,29 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using Microsoft.EntityFrameworkCore;
+using SignalRSample.Data;
 
 namespace SignalRSample.Hubs
 {
 	public class BasicChatHub : Hub
 	{
+		private readonly ApplicationDbContext _dbContext;
+		public BasicChatHub(ApplicationDbContext dbContext)
+		{
+			_dbContext = dbContext;
+		}
+
 		public async Task SendMessage(string sender, string message)
 		{
 			await Clients.All.SendAsync("NewMessageReceived", sender, message);
+		}
+
+		public async Task SendPrivateMessage(string sender, string receiver, string message)
+		{
+			var user = await _dbContext.Users.FirstOrDefaultAsync(u => u.Email.ToLower() == receiver.ToLower());
+			if(user is not null)
+			{
+				await Clients.User(user.Id).SendAsync("NewMessageReceived", sender, message);
+			}
 		}
 	}
 }
