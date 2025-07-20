d7f4601f852a583e0e3c6c27e13f894bc45ce4db
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-07-20 10:10:43 +0500

    NotificationHub initial implementation added.

---
+++ SignalRSample/Hubs/NotificationHub.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRSample.Hubs
+{
+	public class NotificationHub : Hub
+	{
+		public List<string> Notifications { get; set; } = new List<string>();
+		
+		public async Task AddNewNotification(string message)
+		{
+			Notifications.Add(message);
+			await SentCurrentNotificationListToClients();
+		}
+
+		public async Task SentCurrentNotificationListToClients()
+		{
+			await Clients.All.SendAsync("UpdateNotificationListAndCount", Notifications);
+		}
+	}
+}
