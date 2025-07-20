c75e4397b04dcf16960bef63b4041813e27f37e9
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-07-20 16:14:51 +0500

    SendPrivateMessage implemented in basic chat JS on client side.

--- SignalRSample/wwwroot/js/custom/basicchat.js
+++ SignalRSample/wwwroot/js/custom/basicchat.js
@@ -6,15 +6,22 @@ basicChatConnection.on("NewMessageReceived", function (sender, message) {
 	$('#messagesList').append(`<li>${sender} - ${message}</li>`);
 });
 
-$('#sendMessage').on("click", function () {
-
+$('#sendMessage').on("click", function (event) {
 	var sender = $('#senderEmail').val();
 	var message = $('#chatMessage').val();
+	var receiver = $('#receiverEmail').val();
 
-	basicChatConnection.send("SendMessage", sender, message).catch(function (error) {
-		console.log(error);
-	});
-
+	if (receiver.length > 0) {
+		basicChatConnection.send("SendPrivateMessage", sender, receiver, message).catch(function (error) {
+			console.log(error);
+		});
+	}
+	else {
+		basicChatConnection.send("SendMessage", sender, message).catch(function (error) {
+			console.log(error);
+		});
+	}
+	event.preventDefault();
 });
 
 basicChatConnection.start().then(function () {
