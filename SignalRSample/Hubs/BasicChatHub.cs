9eb9c45df73ef75f8f535f5baf073d32fe1bafbf
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-07-20 15:38:39 +0500

    BasicChatHub initial implementation added.

---
+++ SignalRSample/Hubs/BasicChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRSample.Hubs
+{
+	public class BasicChatHub : Hub
+	{
+		public async Task SendMessage(string sender, string message)
+		{
+			await Clients.All.SendAsync("NewMessageReceived", sender, message);
+		}
+	}
+}
