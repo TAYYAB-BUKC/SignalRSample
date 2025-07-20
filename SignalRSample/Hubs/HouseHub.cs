ecabbde47416a2d0b94f19a95853894b3a55c09a
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-07-20 08:12:57 +0500

    HouseHub implementation added.

---
+++ SignalRSample/Hubs/HouseHub.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRSample.Hubs
+{
+	public class HouseHub : Hub
+	{
+		public List<string> HousesJoined { get; set; } = new List<string>();
+
+		public async Task JoinHouse(string houseName)
+		{
+			var houseKey = $"{Context.ConnectionId}:{houseName}";
+			if(!HousesJoined.Contains(houseKey))
+			{
+				HousesJoined.Add(houseKey);
+
+				await Groups.AddToGroupAsync(Context.ConnectionId, houseName);
+			}
+		}
+
+		public async Task LeaveHouse(string houseName)
+		{
+			var houseKey = $"{Context.ConnectionId}:{houseName}";
+			if (HousesJoined.Contains(houseKey))
+			{
+				HousesJoined.Remove(houseKey);
+
+				await Groups.RemoveFromGroupAsync(Context.ConnectionId, houseName);
+			}
+		}
+	}
+}
