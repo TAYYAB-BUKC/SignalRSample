576925c9270cf7b58e54618d95f769688f0c5475
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-07-20 08:24:36 +0500

    House JS initial implementation added.

---
+++ SignalRSample/wwwroot/js/custom/house.js
@@ -0,0 +1,72 @@
+﻿let lbl_houseJoined = document.getElementById("lbl_houseJoined");
+
+let btn_gryffindor = document.getElementById("btn_gryffindor");
+let btn_slytherin = document.getElementById("btn_slytherin");
+let btn_hufflepuff = document.getElementById("btn_hufflepuff");
+let btn_ravenclaw = document.getElementById("btn_ravenclaw");
+
+let btn_un_gryffindor = document.getElementById("btn_un_gryffindor");
+let btn_un_slytherin = document.getElementById("btn_un_slytherin");
+let btn_un_hufflepuff = document.getElementById("btn_un_hufflepuff");
+let btn_un_ravenclaw = document.getElementById("btn_un_ravenclaw");
+
+let trigger_gryffindor = document.getElementById("trigger_gryffindor");
+let trigger_slytherin = document.getElementById("trigger_slytherin");
+let trigger_hufflepuff = document.getElementById("trigger_hufflepuff");
+let trigger_ravenclaw = document.getElementById("trigger_ravenclaw");
+
+var houseConnection = new signalR.CreateHubConnections().withUrl("/hubs/house").build();
+
+houseConnection.start().then(OnHouseSuccessConnection, OnHouseFailedConnection);
+
+function OnHouseSuccessConnection()
+{
+	console.log("Connected to House Hub");
+}
+
+function OnHouseFailedConnection()
+{
+	console.log("Failed to connection to House Hub");
+}
+
+// Subscribe House events
+btn_gryffindor.addEventListener("click", function (event) {
+	houseConnection.send("JoinHouse", "Gryffindor");
+	event.preventDefault();
+});
+
+btn_slytherin.addEventListener("click", function (event) {
+	houseConnection.send("JoinHouse", "Slytherin");
+	event.preventDefault();
+});
+
+btn_hufflepuff.addEventListener("click", function (event) {
+	houseConnection.send("JoinHouse", "Hufflepuff");
+	event.preventDefault();
+});
+
+btn_ravenclaw.addEventListener("click", function (event) {
+	houseConnection.send("JoinHouse", "Ravenclaw");
+	event.preventDefault();
+});
+
+// Unsubscribe House events
+btn_un_gryffindor.addEventListener("click", function (event) {
+	houseConnection.send("LeaveHouse", "Gryffindor");
+	event.preventDefault();
+});
+
+btn_un_slytherin.addEventListener("click", function (event) {
+	houseConnection.send("LeaveHouse", "Slytherin");
+	event.preventDefault();
+});
+
+btn_un_hufflepuff.addEventListener("click", function (event) {
+	houseConnection.send("LeaveHouse", "Hufflepuff");
+	event.preventDefault();
+});
+
+btn_un_ravenclaw.addEventListener("click", function (event) {
+	houseConnection.send("LeaveHouse", "Ravenclaw");
+	event.preventDefault();
+});
