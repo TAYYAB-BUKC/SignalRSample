aa7c8127b2028cf21c8c07f3ecec4fa3c31317d9
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-07-20 07:20:43 +0500

    New client issue resolved as voting status was not displayed for new client.

--- SignalRSample/Hubs/VotingHub.cs
+++ SignalRSample/Hubs/VotingHub.cs
@@ -8,5 +8,15 @@ namespace SignalRSample.Hubs
 		{
 			return StaticDetails.StaticDetails.VoteCounts;
 		}
+
+		public override async Task<Task> OnConnectedAsync()
+		{
+			await Clients.Caller.SendAsync("UpdateVotingStatus",
+			   StaticDetails.StaticDetails.VoteCounts[StaticDetails.StaticDetails.VoterA],
+			   StaticDetails.StaticDetails.VoteCounts[StaticDetails.StaticDetails.VoterB],
+			   StaticDetails.StaticDetails.VoteCounts[StaticDetails.StaticDetails.VoterC]
+		   );
+			return base.OnConnectedAsync();
+		}
 	}
 }
