f3ce78f682e03a3c5b53ab3b42a1123a1c420010
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-07-21 16:06:54 +0500

    User disconnected notification implemented in AdvanceChatHub.

--- SignalRSample/Hubs/AdvanceChatHub.cs
+++ SignalRSample/Hubs/AdvanceChatHub.cs
@@ -24,5 +24,22 @@ namespace SignalRSample.Hubs
 			}
 			return base.OnConnectedAsync();
 		}
+
+		public override async Task<Task> OnDisconnectedAsync(Exception? exception)
+		{
+			var userId = Context?.User?.FindFirstValue(ClaimTypes.NameIdentifier);
+			if (!string.IsNullOrWhiteSpace(userId))
+			{
+				var userEmail = (await _dbContext.Users.FindAsync(userId))?.Email;
+				if (HubHelper.HasUserConnection(userId, Context.ConnectionId))
+				{
+					var userConnections = HubHelper.Users[userId];
+					userConnections.Remove(Context.ConnectionId);
+				}
+				await Clients.Users(HubHelper.OnlineUsers()).SendAsync("UserDisconnected", userEmail);
+			}
+			
+			return base.OnDisconnectedAsync(exception);
+		}
 	}
 }
