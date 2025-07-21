7111433c4f8507f8a2c68cc96207765f0f78f05d
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-07-21 15:17:36 +0500

    AdvanceChatHub initial implementation added.

---
+++ SignalRSample/Hubs/AdvanceChatHub.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using SignalRSample.Data;
+using SignalRSample.Helpers;
+using System.Security.Claims;
+
+namespace SignalRSample.Hubs
+{
+	public class AdvanceChatHub : Hub
+	{
+		private readonly ApplicationDbContext _dbContext;
+		public AdvanceChatHub(ApplicationDbContext dbContext)
+		{
+			_dbContext = dbContext;
+		}
+
+		public override async Task<Task> OnConnectedAsync()
+		{
+			var userId = Context?.User?.FindFirstValue(ClaimTypes.NameIdentifier);
+			if(!string.IsNullOrWhiteSpace(userId))
+			{
+				var userEmail = (await _dbContext.Users.FindAsync(userId))?.Email;
+				await Clients.Users(HubHelper.OnlineUsers()).SendAsync("NewUserConnected", userId, userEmail);
+				HubHelper.AddUserConnection(userId, Context.ConnectionId);
+			}
+			return base.OnConnectedAsync();
+		}
+	}
+}
