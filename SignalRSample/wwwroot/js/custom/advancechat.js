32dc351857e32d6d6f954d9bf8c8af1e18c01620
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-07-21 15:40:56 +0500

    Advance chat JS initial implementation added.

---
+++ SignalRSample/wwwroot/js/custom/advancechat.js
@@ -0,0 +1,18 @@
+﻿
+var advanceChatConnection = new signalR.HubConnectionBuilder().withUrl("/hubs/advancechat").build();
+
+advanceChatConnection.on("NewUserConnected", function (userId, userEmail) {
+	AddMessage(`${userEmail} is online!`);
+});
+
+function AddMessage(message) {
+	if (message != '' && message != null) {
+		$('#messagesList').append(`<li>${message}</li>`);
+	}
+}
+
+advanceChatConnection.start().then(function OnSuccess() {
+	console.log("Connected to Advance Chat Hub");
+}, function OnFailure() {
+	console.log("Failed to connect to Advance Chat Hub");
+});
