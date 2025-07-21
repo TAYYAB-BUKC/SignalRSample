16ac8688b25fb337ffbc64c0c88e139fa43a6049
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-07-21 14:52:43 +0500

    ChatViewModel added

---
+++ SignalRSample/Models/ChatViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace SignalRSample.Models
+{
+	public class ChatViewModel
+	{
+		public ChatViewModel()
+		{
+			Rooms = new List<ChatRoom>();
+			MaxRoomsAllowed = 4;
+		}
+
+		public int MaxRoomsAllowed { get; set; }
+		public List<ChatRoom> Rooms { get; set; }
+		public string? UserId { get; set; }
+		public bool IsRoomAllowedToAdd => Rooms is null || Rooms.Count < MaxRoomsAllowed;
+	}
+}
